b508710b6f6420ec1bef8352bc093751afc31c10
Florian Schatz <schatzflorian@yahoo.com>
2019-11-05 19:09:14 +0100

    Paste of old Code

--- vec_refac/vec_refac/main.cpp
+++ vec_refac/vec_refac/main.cpp
@@ -1,5 +1,57 @@
 #include <iostream>
+#include <chrono>
+#include <fstream>
+#include <vector>
+#include "vector.h"
+
+void test(int);
+void test2(int);
 int main() {
-	std::cout << "Das ist ein Testprogramm!" << std::endl;
-	return 0;
+	for (int i = 10; i < 100000; i *= 10) {
+		test(i);
+	}
+	std::cout << "fertig\n";
+}
+
+void test(int number) {
+	vector <int> Vektor;
+
+	auto t1 = std::chrono::steady_clock::now(); //Aktueller Zeitpunkt wird gespeichert
+
+	for (int i = number; i > 0; i--) {
+		Vektor.pushback(i);
+	}
+
+	auto t2 = std::chrono::steady_clock::now();// Zeitpunkt nach Ausführung von push_back wird gespeichert
+	auto d_nano = std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count();//Zeitspanne wird in Microsekunden ermittelt
+
+	//Vektor.~vector();
+
+	std::ofstream data;
+	std::string name = "data_25.csv";
+
+	data.open(name, std::ofstream::app);
+	data << number << ";" << d_nano / number << ";" << EXTRA_LENGTH << "\n";
+	data.close();
+
+}
+
+void test2(int number) {
+	std::vector<int> vec;
+	auto t1 = std::chrono::steady_clock::now(); //Aktueller Zeitpunkt wird gespeichert
+
+	for (int i = number; i > 0; i--) {
+		vec.push_back(i);
+	}
+
+	auto t2 = std::chrono::steady_clock::now();// Zeitpunkt nach Ausführung von push_back wird gespeichert
+	auto d_nano = std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count();//Zeitspanne wird in Microsekunden ermittelt
+	
+	std::ofstream data;
+	std::string name = "data_vec.csv";
+
+	data.open(name, std::ofstream::app);
+	data << number << ";" << d_nano / number << ";" << "0" << "\n";
+	data.close();
+	
 }
---
+++ vec_refac/vec_refac/vector.h
@@ -0,0 +1,103 @@
+#pragma once
+
+//Länge des "Buffers"
+#define EXTRA_LENGTH 50
+
+
+template <class T>
+class vector
+{
+private:
+	T* vec = new T[50];			//"Start"Vektor
+	int used_fields = 0;		//Anzahl genützter Datenfelder im Vektor
+	int length = 50;			//Physikalische Länge des Vektors
+public:
+	vector() {};
+	~vector();
+	void resize(int);			//verkürzt Vektor auf übergebene Länge
+	void popback();				//entfernt letztes Element
+	void pushback(T);			//fügt am Ende des Vektors übergebenen Wert ein
+	void clear();				//setzt alle benützten Datenfelder des Vektors auf null
+	int size();					//gibt Länge des physikalischen Vektors zurück
+	T at(int);					//gibt Inhalt an übergebener Stelle zurück
+};
+
+
+template<class T>
+vector<T>::~vector() {
+	//Destruktor löscht array
+	delete[] vec;
+}
+
+template<class T>
+void vector<T>::clear() {
+	for (int i = 0; i < used_fields; ++i) {
+		//Inhalt des Vektors elementweise nullen
+		vec[i] = 0;
+	}
+	//Vektor auf Bufferlänge kürzen
+	resize(EXTRA_LENGTH);
+	used_fields = 0;
+}
+
+template<class T>
+void vector<T>::resize(int nu_length) {
+	if (nu_length != length) {
+		//Übergebene Länge entspricht nicht der bereits Vorhandenen
+		//neues Array allozieren
+		T* nu_vector = new T[nu_length];
+		for (int i = 0; i < length; i++) {
+			if (i < nu_length) {
+				//Elementweise kopieren
+				nu_vector[i] = vec[i];
+			}
+		}
+		if (nu_length < used_fields) {
+			//Vektor wurde so gekürzt, dass Daten verloren gegangen sind -> Nutzdatenlänge anpassen
+			used_fields = nu_length;
+		}
+		// Gesamtlänge des Vektors anpassen und alten Vektor löschen
+		length = nu_length;
+		delete[] vec;
+		//Zeiger auf neuen Vektor umbiegen
+		vec = nu_vector;
+	}
+}
+
+template<class T>
+void vector<T>::popback() {
+	//letzteS Element nullen und physikalische Länge decrementieren
+	vec[used_fields - 1] = 0;
+	used_fields--;
+	//Wenn mehr als 2* EXTRA_LENGTH freie Felder, dann Vektor automatisch schrumpfen auf EXTRA_LENGTH Felder als Buffer
+	if ((used_fields + 2 * EXTRA_LENGTH) <= length) {
+		resize(used_fields + EXTRA_LENGTH);
+	}
+}
+
+template<class T>
+void vector<T>::pushback(T value) {
+	//bei Bedarf Vekotr verlängern
+	if (used_fields >= length) {
+		resize(used_fields + EXTRA_LENGTH);
+	}
+	//Wert in das erste freie Feld des Vektors schreiben und physikalische Länge anpassen
+	vec[used_fields] = value;
+	used_fields++;
+}
+
+template<class T>
+int vector<T>::size() {
+	//gibt physikalische Länge zurück
+	return used_fields;
+}
+
+template<class T>
+T vector<T>::at(int pos) {
+	//gibt Wert an pos zurück; falls pos nicht im Vektor wird NULL returned
+	if (pos < used_fields) {
+		return vec[pos];
+	}
+	return nullptr;
+}
+
